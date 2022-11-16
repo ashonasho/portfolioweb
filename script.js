068ba8e3701a8542c07f3e8020b04f32f80fd44d
SajithJude <judesajith.aj@gmail.com>
2022-11-17 01:58:28 +0530

    new

--- script.js
+++ script.js
@@ -6,14 +6,14 @@
 
 const scene = new THREE.Scene();
 
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const camera = new THREE.PerspectiveCamera(75, withd / hait, 0.1, 1000);
 
 const renderer = new THREE.WebGLRenderer({
   canvas: document.querySelector('#bg'),
 });
 
 renderer.setPixelRatio(window.devicePixelRatio);
-renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setSize(withd, hait);
 camera.position.setZ(30);
 camera.position.setX(-3);
 
